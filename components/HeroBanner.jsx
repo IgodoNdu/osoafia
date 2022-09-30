0ec5551e3f8a36020786cfa0305d4a7b5c55424e
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-30 13:29:38 +0100

    Base structure for HeroBanner Component and the home page
    Styling out the base structure for the HeroBanner component, and the Home Page (index.js file)

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,9 +1,27 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HeroBanner = () => {
   return (
-    <div>
-      
+    <div className='hero-banner-container'>
+      <div>
+        <p className='beats-solo'>SMALL TEXT</p>
+        <h3>Mid Text</h3>
+        <img src='' alt='headphones' className='hero-banner-image' />
+
+        <div>
+          <Link href='/product/:id'>
+            <button type='button'>Dynamic Button Text</button>
+          </Link>
+          <div className='desc'>
+            <h5>Description</h5>
+            <p>THE DESCRIPTION</p>
+          </div>
+        </div>
+
+      </div>
+
+
     </div>
   )
 }
