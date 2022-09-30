5d4260ef36f52755fd2edd1fc361c730baecb9f9
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-30 15:23:43 +0100

    Updating to fetch products and banner props from sanity
    importing the sanity client for use in the homepage for fetching/getting (products and banner) data/props from sanity, with use of the 'getServerSideProps' async function. Then pass the retrieved/fetched data as props to the requisite components: (Here: passing the banner props to the HeroBanner Component)

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,49 @@
 import React from 'react';
+
+//for using sanity client in the project
+import { client } from '../lib/client';
+
 //importing the components I'll be needing here
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
       
-      <HeroBanner />
-
+      {/**If bannerData exists, return the 0 index (as can be seen via console.log, [0] has get the needed data) */}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many varieties</p>
       </div>
 
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map((product) => product)}
+        {/**Loop over the products (received as a prop from sanity) to return each product's name */}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
     </>
   )
+}
+
+//for next js getting/fetching data/props
+export const getServerSideProps = async () => {
+  //forming a sanity query to fetch all products from my sanity dashboard
+  const query = '*[_type == "product"]';
+  //hold the result of the query in the products const
+  const products = await client.fetch(query);
+
+  //to fetch all banners from my sanity
+  const bannerQuery = '*[_type == "banner"]';
+  //hold the result of the query in the banner const
+  const bannerData = await client.fetch(bannerQuery);
+
+  //return the fetched result (will be passed as props to the app components)
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home;
