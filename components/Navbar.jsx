43809edb18728f0fe9da3f28f32e869c385bb22e
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-02 00:28:15 +0100

    Logic for Opening and closing the cart via button click
    Using the setShowCart, and showCart state from Context Provider, to  open/close the cart via a button click

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,16 +4,25 @@ import Link from 'next/link'
 //for the shopping icon
 import { AiOutlineShopping } from 'react-icons/ai'
 
+import Cart from './Cart'
+//pull the setShowCart & other State(s) I'll be needing here
+import { useStateContext } from '../context/StateContext'
+
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <div className='navbar-container'>
       <p className='logo'>
         <Link href='/'>OsoAfia Headphones</Link>
       </p>
 
-      <button type='button' className='cart-icon'>
-        <AiOutlineShopping /><span className='cart-item-qty'>0</span>
+      {/**Making the button close and open the cart on demand */}
+      <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
+        <AiOutlineShopping /><span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+
+      {/**Then Only show the cart component, when the ShowCart State is set to true */}
+      {showCart && <Cart />}
     </div>
   )
 }
