89df9648d4fe72b5313bf4935c012e7242344eef
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-01 09:39:22 +0100

    Layout Component update
    Building out the layout structure, and using the {children} prop to render every other component (excluding the <Navbar /> and <Footer /> components: body)

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+//as is with the head tag for html
+import Head from 'next/head'
 
-const Layout = () => {
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
   return (
-    <div>
-      
+    <div className='layout'>
+      <Head>
+        <title>Alexander's Test Store</title>
+      </Head>
+      {/**Using the html5 semantic tags */}
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   )
 }
