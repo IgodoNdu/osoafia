38550643d731c7aaf4f8dbf942e475cfc034422e
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-01 09:50:23 +0100

    Navbar Component update
    Building the Navbar component, with the shopping cart icon and the logo <p> tag

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import Link from 'next/link'
+//for the shopping icon
+import { AiOutlineShopping } from 'react-icons/ai'
+
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>OsoAfia Headphones</Link>
+      </p>
+
+      <button type='button' className='cart-icon'>
+        <AiOutlineShopping /><span className='cart-item-qty'>0</span>
+      </button>
     </div>
   )
 }
