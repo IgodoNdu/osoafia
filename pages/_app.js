51e59da95b83182bf71ae5e0f854fa8d257d1ff9
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-01 09:35:35 +0100

    Applying the Layout structure across the entire app
    Wrapping the entire application with the Layout component, hence the layout structure now applies to every sub-component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import React from 'react'
 import '../styles/globals.css'
 
+//implementing the layout on the App (over all the components)
+import { Layout } from '../components'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
